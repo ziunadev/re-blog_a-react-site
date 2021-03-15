3dbbbb05eef0b034a36046b7177828d7aebbcc92
Putra Krishna <krishna.putra332@gmail.com>
2021-03-15 09:13:09 +0700

    Update Fetch.js

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,26 +1,26 @@
 import React from 'react';
+import Santri from './Santri'
 
 export default class Fetch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       listSantri: [],
-<<<<<<< HEAD
       listSantriResult: null,
-      name: "",
-      division: "",
-      origin: "",
-=======
       name: '',
       division: '',
       origin: '',
->>>>>>> e71a8800f13b6f4b3fc14287e5a35f7da0396498
       isLoading: false,
-      result: null
+      result: null,
+      getEditedDataResult: null,
+      deleteSantriResult: null
     }
 
     this.submitSantri = this.submitSantri.bind(this);
-    this.getListSantri = this.getListSantri.bind(this);
+    this.getDataSantri = this.getDataSantri.bind(this);
+    this.renderHere = this.renderHere.bind(this);
+    this.updateSantri = this.updateSantri.bind(this);
+    this.deleteSantri = this.deleteSantri.bind(this);
   }
 
   submitSantri() {
@@ -28,15 +28,9 @@ export default class Fetch extends React.Component {
       {isLoading: true},
       () => {
         const data = new FormData();
-<<<<<<< HEAD
-        data.append("name", this.state.name);
-        data.append("division", this.state.division);
-        data.append("origin", this.state.origin);
-=======
         data.append('name', this.state.name);
         data.append('division', this.state.division);
         data.append('origin', this.state.origin);
->>>>>>> e71a8800f13b6f4b3fc14287e5a35f7da0396498
 
         fetch(
           'http://localhost:3030/api/v1/santri',
@@ -44,38 +38,22 @@ export default class Fetch extends React.Component {
             method: 'POST',
             body: data
           }
-<<<<<<< HEAD
-        )
-        .then(async (resp) => {
-          var result;
-          if (resp.status == 200) {
-            result = 'ok';
-            this.getListSantri()
-          } else {
-            result = 'error';
-          }
-          this.setState({result: result, isLoading: false})
-        })
-        .catch((err) => {
-          this.setState({result: 'error'})
-=======
         ).then((resp) => {
           var result;
           if (resp.status == 200) {
             result = 'ok';
           } else {
-            result = `error: ${resp.status}`;
+            result = `error : ${resp.status}`
           }
-          this.setState({result: result, isLoading: false});
+          this.setState({result: result, isLoading: false})
         }).catch((err) => {
-          this.setState({result: `error : ${err.status}`})
->>>>>>> e71a8800f13b6f4b3fc14287e5a35f7da0396498
+          this.setState({result: `error : ${err.status}`, isLoading: false})
         })
       }
     )
   }
 
-  getListSantri() {
+  getDataSantri() {
     fetch(
       'http://localhost:3030/api/v1/santri'
     )
@@ -84,6 +62,7 @@ export default class Fetch extends React.Component {
         listSantri: null,
         listSantriResult: null
       }
+
       if (resp.status == 200) {
         const body = await resp.json();
         result.listSantri = body.data;
@@ -92,13 +71,60 @@ export default class Fetch extends React.Component {
         result.listSantri = [];
         result.listSantriResult = 'error';
       }
-      this.setState(result)
+      this.setState(result);
     })
     .catch((err) => {
       this.setState({
         listSantri: [],
         listSantriResult: 'error'
       })
+    })
+  }
+
+  updateSantri(id, data) {
+    const body = new FormData();
+    body.append('name', data.name);
+    body.append('division', data.division);
+    body.append('origin', data.origin);
+
+    fetch(
+      'http://localhost:3030/api/v1/santri/'+id,
+      {
+        method: 'PUT',
+        body: body
+      }
+    ).then((resp) => {
+      var result;
+      if (resp.status == 200) {
+        this.getDataSantri();
+        result = 'ok';
+      } else {
+        result = 'error';
+      }
+      this.setState({getEditedDataResult: result})
+    })
+    .catch((err) => {
+      this.setState({getEditedDataResult: 'error'})
+    })
+  }
+
+  deleteSantri(id) {
+    fetch(
+      'http://localhost:3030/api/v1/santri/'+id,
+      {
+        method: 'DELETE'
+      }
+    ).then((resp) => {
+      var result;
+      if (resp.status == 204) {
+        this.getDataSantri();
+        result = 'delete successfull';
+      } else {
+        result = `delete failure, error : ${resp.status}`;
+      }
+      this.setState({deleteSantriResult: result});
+    }).catch((err) => {
+      this.setState({deleteSantriResult: 'error deleting current data'});
     })
   }
 
@@ -126,46 +152,49 @@ export default class Fetch extends React.Component {
             type="button"
             onClick={this.submitSantri}
           >
-            Simpan data
+            Simpan
           </button>
           <button
             type="button"
-            onClick={this.getListSantri}
+            onClick={this.getDataSantri}
           >
-            Ambil Data Santri
+            Ambil
           </button>
-          <div>
-            {this.state.isLoading == true ?
-              (
-                <h1>Loading</h1>
-              ) : null}
-          </div>
-          <div>
-            {this.renderHere()}
-          </div>
-          <div>
-            <ul>
-            {this.state.listSantri.map((el) => <li>{JSON.stringify(el)}</li>)}
-            </ul>
-          </div>
-          <button
-            type="button"
-            onClick={this.getListSantri}
-          >
-            Ambil Data
-          </button>
-          <h1>{this.state.result}</h1>
         </form>
+        <div>
+          {
+            this.state.isLoading == true ?
+            (
+              <h1>Loading</h1>
+            ) : null
+          }
+        </div>
+        {this.state.result}
+        {this.renderHere()}
+        <table>
+          <tr>
+            <td>Nama&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            <td>Divisi&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            <td>Alamat&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          </tr>
+          {this.state.listSantri.map(
+            (el) => <Santri
+                      santri={el}
+                      key={el.id}
+                      onUpdate={this.updateSantri}
+                      onDelete={this.deleteSantri}
+                    />
+          )}
+        </table>
       </div>
     )
   }
-
   renderHere() {
     if (this.state.result == 'ok') {
       return(
         <h2>Berhasil</h2>
       )
-    } else if (this.state.result == 'error') {
+    } else if (this.state.result != 'ok' && this.state.result != null) {
       return(
         <h2>Error</h2>
       )
